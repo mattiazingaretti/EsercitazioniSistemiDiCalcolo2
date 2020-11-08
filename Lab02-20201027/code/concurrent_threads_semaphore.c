1d495a04cb9bac6be965b1c6aeb87d79f360a7b3
mattiazingaretti <zingaretti.mattia@gmail.com>
2020-11-08 18:12:54 +0100

    edit

--- Lab02-20201027/code/concurrent_threads_semaphore.c
+++ Lab02-20201027/code/concurrent_threads_semaphore.c
@@ -3,9 +3,10 @@
 #include <pthread.h>
 #include <errno.h>
 #include <semaphore.h> 
+#include "performance.h"
 
-#define N 10//1000 // number of threads
-#define M 10 //10000 // number of iterations per thread
+#define N 1000 // number of threads
+#define M 10000 // number of iterations per thread
 #define V 1 // value added to the balance by each thread at each iteration
 
 unsigned long int shared_variable;
@@ -17,13 +18,14 @@ sem_t * my_sem;
 void* thread_work(void *arg) {
 	int i;
 	for (i = 0; i < m; i++)
-		
+		//printf("OUT area critica\n");
 		if (sem_wait(my_sem) != 0){
 			printf("Errore sulla sem_wait !");
 			return NULL;
 		}
 		//Inizio area critica
 		shared_variable += v;
+		//printf("in area critica\n");
 		//Fine area critica
 		if (sem_post(my_sem) != 0){
 			printf("Errore sulla sempost!");
@@ -47,7 +49,7 @@ int main(int argc, char **argv)
 	my_sem = (sem_t*) malloc(sizeof(sem_t));
 	sem_init(my_sem, 0 , 1);
 	
-	begin(&t);
+	begin(&myTimer);
 	int i;
 	for (i = 0; i < n; i++)
 		if (pthread_create(&threads[i], NULL, thread_work, NULL) != 0) {
@@ -59,7 +61,7 @@ int main(int argc, char **argv)
 	printf("Waiting for the termination of all the %d threads...", n); fflush(stdout);
 	for (i = 0; i < n; i++)
 		pthread_join(threads[i], NULL);
-	end(&t);
+	end(&myTimer);
 	printf("ok\n");
 	
 	
@@ -69,10 +71,11 @@ int main(int argc, char **argv)
 		unsigned long int lost_adds = (expected_value - shared_variable) / v;
 		printf("Number of lost adds: %lu\n", lost_adds);
 	}
-
+	
+	sem_destroy(my_sem);	
     free(threads);
 	//Dopo che tutti i threads hanno terminato posso deallocare il mio semaforo globale.
-	sem_destroy(my_sem);
+
 	free(my_sem);
 	
 	return EXIT_SUCCESS;
