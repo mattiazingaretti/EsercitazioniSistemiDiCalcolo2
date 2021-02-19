a01507ad4431293084d47bc411669a30b0909094
mattiazingaretti <zingaretti.mattia@gmail.com>
2021-02-19 16:46:25 +0100

    Completed Excersice 2 of Lab01. Noticing that all the computation is made up with local variables, or pointers to a disjointed allocated memory space. (I mean the i-th element of the BUF structure).

--- Lab01/code/concurrent_threads.c
+++ Lab01/code/concurrent_threads.c
@@ -13,16 +13,19 @@ unsigned long int shared_variable;
 int n = N, m = M, v = V;
 
 //Global structure
-int* BUF;
+int** BUF;
 
 void* thread_work(void *arg) {
 	
-	int* index = (int*) arg;
+	int *total_arg = (int*) arg;
+
 	//DEBUG printf("Thread %d doing stuff \n", *index);
 	int i, local = 0; 
 	for (i = 0; i < m; i++)
 		local += v;
-	BUF[*index] = local; 
+	
+	*total_arg = local; 
+	
 	return NULL;
 }
 
@@ -34,29 +37,35 @@ int main(int argc, char **argv)
 	shared_variable = 0;
 
 	//Init Global BUF
-	BUF = (int*) calloc(N, sizeof(int));
+	BUF = (int**) malloc(N*sizeof(int*));
 
 	printf("Going to start %d threads, each adding %d times %d to a shared variable initialized to zero...", n, m, v); fflush(stdout);
 	pthread_t* threads = (pthread_t*)malloc(n * sizeof(pthread_t)); // also calloc(n,sizeof(pthread_t))
 	int i;
 	for (i = 0; i < n; i++)
+	{
+		BUF[i] = (int*) calloc(1,sizeof(int)); 
 		
-		if (pthread_create(&threads[i], NULL, thread_work, &i) != 0) {
+		if (pthread_create(&threads[i], NULL, thread_work, BUF[i]) != 0)
+		{
 			fprintf(stderr, "Can't create a new thread, error %d\n", errno);
 			exit(EXIT_FAILURE);
 		}
+	}
 	printf("ok\n");
 
+
+	
 	printf("Waiting for the termination of all the %d threads...", n); fflush(stdout);
 	for (i = 0; i < n; i++)
 		pthread_join(threads[i], NULL);
 	printf("ok\n");
 
-
 	for(int j = 0 ; j < n ; j++){
-		printf("Buff elem %d = %d \n",j, BUF[j]);
-		shared_variable +=  BUF[j];
+		//DEBUG printf("Buff elem %d = %d \n",j, *BUF[j]);
+		shared_variable +=  *BUF[j];
 	}
+
 
 	unsigned long int expected_value = (unsigned long int)n*m*v;
 	printf("The value of the shared variable is %lu. It should have been %lu\n", shared_variable, expected_value);
